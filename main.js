1a11d9ed8e77b26a18ea8149d0362d3c7fbb898a
unwantet <darkblezer59@gmail.com>
2023-11-30 18:53:05 +0500

    1-hw

---
+++ main.js
@@ -0,0 +1,174 @@
+// function sum(x,y) {
+//     console.log(x+y);
+// }
+// sum(5,6);
+
+
+
+
+// function toSekunds(minutes) {
+//     console.log(minutes * 60);
+// }
+
+// toSekunds(20)
+
+
+
+
+
+
+// function nextInteger(int) {
+//     console.log(int + 1);
+// }
+// nextInteger(6)
+
+
+
+
+
+// function uchburchakYuzi(asos, balandlik) {
+//     let S = (asos * balandlik) / 2;
+//     console.log(S);
+// }
+
+// uchburchakYuzi(12,8)
+
+
+
+
+
+// function ageToDays(age) {
+//     console.log(age * 365);
+// }
+
+// ageToDays(65)
+
+
+
+
+
+// function kub(int) {
+//     let a = int ** 3;
+//     console.log(a);
+// }
+
+// kub(12);
+    
+
+
+
+// function birinchiElementniQaytar(massiv) {
+//     if (massiv && massiv.length > 0) {
+//         return massiv[0];
+//     } else {
+//         return null;
+//     }
+// }
+
+// let massivim = [3, 7, 1, 9, 4];
+// let natija = birinchiElementniQaytar(massivim);
+// console.log( natija);
+
+
+
+
+// function power(x,y) {
+//     console.log(x*y);    
+// }
+
+// power(6,8)
+
+
+
+
+
+
+// function hourToSekunds(hour) {
+//     let s = hour * 3600;
+//     console.log(s);
+// }
+
+// hourToSekunds(2)
+
+
+
+
+
+
+// function uchinchiTomon(tomon1, tomon2) {
+//     let max = (tomon1 + tomon2) - 1;
+//     console.log(max);
+// }
+// uchinchiTomon(12,48)
+
+
+
+
+
+// function qoldiqniTop(son1, son2) {
+//     if (son2 !== 0) {
+//         return son1 % son2;
+//     } else {
+//         console.error("Qoldiqni hisoblash uchun ikkinchi son 0 bo'lishi mumkin emas.");
+//         return null;
+//     }
+// }
+
+
+// let birinchiSon = 17;
+// let ikkinchiSon = 5;
+
+// let qoldiq = qoldiqniTop(birinchiSon, ikkinchiSon);
+
+// if (qoldiq !== null) {
+//     console.log(`${birinchiSon} / ${ikkinchiSon} = ${birinchiSon / ikkinchiSon}, Qoldiq: ${qoldiq}`);
+// }
+
+
+
+
+
+
+// function turiburchakYuzi(boyi, eni) {
+//     if (boyi <= 0 || eni <= 0) {
+//         console.error("Bo'yi va eni musbat bo'lishi shart.");
+//         return null;
+//     } else {
+//         return boyi * eni;
+//     }
+// }
+// let boyi = 8;
+// let eni = 12;
+
+// let yuzi = turiburchakYuzi(boyi, eni);
+
+// if (yuzi !== null) {
+//     console.log(`Turiburchakning bo'yi: ${boyi}, eni: ${eni}, yuzi: ${yuzi}`);
+// }
+
+
+
+
+
+
+// function birlashtirishFunksiyasi(a) {
+//     return "Something " + a;
+// }
+
+
+// let malumot = "Awesome";
+// let natija = birlashtirishFunksiyasi(malumot);
+
+// console.log(natija);
+
+
+
+
+
+// function kv(num) {
+//     console.log(num ** 2);
+// }
+
+// kv(35)
+
+
